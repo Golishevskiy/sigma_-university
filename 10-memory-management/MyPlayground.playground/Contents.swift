dc6f5e5444e76779c375498ee72be322495c0f65
Petro Golishevskiy <golishevskiypetro@ukr.net>
2021-04-17 00:14:06 +0300

    task 1 finished

---
+++ 10-memory-management/MyPlayground.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+class Parent {
+    let name: String
+    var son: Son?
+    init(name: String) {
+        self.name = name
+        print("Parent is being initialized")
+    }
+    
+    deinit {
+        print("Parent is being deinitialized")
+    }
+}
+
+class Son {
+    weak var parent: Parent?
+    init(parent: Parent) {
+        self.parent = parent
+        print("Son is being initialized")
+    }
+    
+    deinit {
+        print("Son is being deinitialized")
+    }
+}
+
+do {
+    let parent = Parent(name: "SomeName")
+    let son = Son(parent: parent)
+    parent.son = son
+}
+
+
