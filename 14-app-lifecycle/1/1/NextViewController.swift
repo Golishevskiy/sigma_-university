499d011f01e907e0df1ef628ca5bb7931241ba7c
Petro Golishevskiy <golishevskiypetro@ukr.net>
2021-05-06 16:49:09 +0300

    life cycle app

---
+++ 14-app-lifecycle/1/1/NextViewController.swift
@@ -0,0 +1,18 @@
+//
+//  NextViewController.swift
+//  1
+//
+//  Created by Petro Golishevskiy on 06.05.2021.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+    override func viewDidLoad() {
+        print("viewDidLoad NextViewController")
+    }
+    
+    deinit {
+        print("deinit NextViewController")
+    }
+}
---
+++ 14-app-lifecycle/1/1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  1
+//
+//  Created by Petro Golishevskiy on 05.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("viewDidLoad ViewController")
+        // Do any additional setup after loading the view.
+    }
+    
+    deinit {
+        print("deinit ViewController")
+    }
+
+
+}
+
