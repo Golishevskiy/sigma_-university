1c61ca625b81b2f36747069e0d50385357e7d9a0
Petro Golishevskiy <golishevskiypetro@ukr.net>
2021-06-02 23:45:34 +0300

    init commit

---
+++ 22-networking/HW22/HW22/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HW22
+//
+//  Created by Petro Golishevskiy on 02.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
