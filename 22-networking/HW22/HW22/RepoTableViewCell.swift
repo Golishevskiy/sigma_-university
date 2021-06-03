bf8a9122fe2b5fcb434267ca1434dc3f9790e03d
Petro Golishevskiy <golishevskiypetro@ukr.net>
2021-06-03 23:46:15 +0300

    load image

---
+++ 22-networking/HW22/HW22/RepoTableViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  RepoTableViewCell.swift
+//  HW22
+//
+//  Created by Petro Golishevskiy on 03.06.2021.
+//
+
+import UIKit
+
+class RepoTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var repoImageView: UIImageView!
+    @IBOutlet weak var repoNameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    func setup(name: String, description: String, imgURL: String) {
+        repoNameLabel.text = name
+        descriptionLabel.text = description
+        guard let url = URL(string: imgURL) else { return }
+        repoImageView.load(url: url)
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 22-networking/HW22/HW22/RepoTableViewController.swift
@@ -0,0 +1,119 @@
+//
+//  RepoTableViewController.swift
+//  HW22
+//
+//  Created by Petro Golishevskiy on 02.06.2021.
+//
+
+import UIKit
+
+class RepoTableViewController: UITableViewController {
+    let net = Network()
+    private var repositories = [Any]()
+    var identifier = "cell"
+    private var items: [Item]? {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        net.search(page: "1",
+                   searchText: "Alamofire") { [weak self] (items) in
+            self?.items = items
+        }
+        setupSearchBar()
+        
+        tableView.register(UINib(nibName: "RepoTableViewCell", bundle: nil),
+                           forCellReuseIdentifier: "cell")
+    }
+    
+    func setupSearchBar() {
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let items = self.items else {
+            return 0
+        }
+        return items.count
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier,
+                                                 for: indexPath) as! RepoTableViewCell
+        
+        guard let items = self.items else { return UITableViewCell() }
+        let item = items[indexPath.row]
+        cell.setup(name: item.fullName,
+                   description: item.itemDescription,
+                   imgURL: item.owner.avatarURL)
+
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension RepoTableViewController: UISearchResultsUpdating {
+  func updateSearchResults(for searchController: UISearchController) {
+    print("searchController")
+  }
+}
