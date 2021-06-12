a213d15bd7c2c165ebab42dcf071ba1be83a9932
Petro Golishevskiy <golishevskiypetro@ukr.net>
2021-06-12 11:37:55 +0300

    init commit

---
+++ 24-debugging/HW24/HW24/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  HW24
+//
+//  Created by Petro Golishevskiy on 12.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
