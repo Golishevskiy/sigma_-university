a513e0758a5223ceba36825e82c93a1b2caecfb7
Petro Golishevskiy <golishevskiypetro@ukr.net>
2021-06-12 14:15:37 +0300

    greaterThan5&containsC breakpoint

--- 24-debugging/HW24/HW24/ViewController.swift
+++ 24-debugging/HW24/HW24/ViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var array = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        fillArray()
+    }
+    
+    private func fillArray() {
+        for _ in 1...100 {
+            let someString = randomString()
+            array.append(someString)
+        }
+    }
+    
+    private func randomString() -> String {
+        let randomInt = Int.random(in: 0..<20)
+        let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_)(*?:%;№"
+        return String((0..<randomInt).map{ _ in letters.randomElement()! })
     }
 }
 
