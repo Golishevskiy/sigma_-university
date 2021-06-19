3fd01e21a668f18dee6aea7db61a4718f308b0b0
Petro Golishevskiy <golishevskiypetro@ukr.net>
2021-06-20 00:17:01 +0300

    before changed supervire button

--- final-project/Currency/Currency/DetailViewController.swift
+++ final-project/Currency/Currency/DetailViewController.swift
@@ -10,18 +10,91 @@ import UIKit
 class DetailViewController: UITableViewController {
     
     var bank: Exchanger?
+    
+    let phoneButton = SharedButton(diameter: 60,
+                                   imageName: "phone",
+                                   tintColor: .grey,
+                                   bgColor: .white)
+    let locationButton = SharedButton(diameter: 60,
+                                      imageName: "location",
+                                      tintColor: .grey,
+                                      bgColor: .white)
+    let linkButton = SharedButton(diameter: 60,
+                                  imageName: "link",
+                                  tintColor: .grey,
+                                  bgColor: .white)
+    let button = SharedButton(diameter: 60,
+                              imageName: "menu2",
+                              tintColor: .white,
+                              bgColor: .green,
+                              shadow: true)
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = bank?.name
         
-//        let button = SharedButton(width: 100, height: 100)
-//        button.frame.size = CGSize(width: 100, height: 100)
-//        view.addSubview(button)
+        button.addTarget(self, action: #selector(menuClicked), for: .touchUpInside)
         
-//        let saerchImage = UIImage(named: "linkCopy")
-//        let tintedSearchImage = saerchImage?.withRenderingMode(.alwaysTemplate)
-//        navigationItem.rightBarButtonItem = UIBarButtonItem(image: tintedSearchImage, style: .plain, target: nil, action: nil)
+        self.navigationController?.view.addSubview(phoneButton)
+        self.navigationController?.view.addSubview(locationButton)
+        self.navigationController?.view.addSubview(linkButton)
+        self.navigationController?.view.addSubview(button)
+                
+        phoneButton.isHidden = true
+        locationButton.isHidden = true
+        linkButton.isHidden = true
+        
+//        if let nav = self.navigationController {
+//            button.trailingAnchor.constraint(equalTo: nav.view.trailingAnchor, constant: -19).isActive = true
+//            button.bottomAnchor.constraint(equalTo: nav.view.bottomAnchor, constant: -19).isActive = true
+//
+//            phoneButton.trailingAnchor.constraint(equalTo: nav.view.trailingAnchor, constant: -19).isActive = true
+//            phoneButton.bottomAnchor.constraint(equalTo: nav.view.bottomAnchor, constant: -19).isActive = true
+//
+//            locationButton.trailingAnchor.constraint(equalTo: nav.view.trailingAnchor, constant: -19).isActive = true
+//            locationButton.bottomAnchor.constraint(equalTo: nav.view.bottomAnchor, constant: -19).isActive = true
+//
+//            linkButton.trailingAnchor.constraint(equalTo: nav.view.trailingAnchor, constant: -19).isActive = true
+//            linkButton.bottomAnchor.constraint(equalTo: nav.view.bottomAnchor, constant: -19).isActive = true
+//        }
+        
+        
+    }
+
+    
+    @objc func menuClicked() {
+        tableView.alpha = 0.5
+        showMenu()
+    }
+    
+    private func showMenu() {
+        if phoneButton.isHidden == true {
+            UIView.animate(withDuration: 0.3) {
+                self.phoneButton.isHidden = false
+                self.locationButton.isHidden = false
+                self.linkButton.isHidden = false
+                
+                self.phoneButton.transform = CGAffineTransform(translationX: .zero, y: -(self.phoneButton.frame.width + 12))
+                self.locationButton.transform = CGAffineTransform(translationX: .zero, y: -((self.phoneButton.frame.width + 12) * 2))
+                self.linkButton.transform = CGAffineTransform(translationX: .zero, y: -((self.phoneButton.frame.width + 12) * 3))
+                
+                self.button.setImage(UIImage(named: "close"), for: .normal)
+                self.button.setImageTintColor(UIColor(hexFromString: "FFFFFF"))
+            }
+        } else {
+            UIView.animate(withDuration: 0.3) {
+                self.phoneButton.transform = CGAffineTransform(translationX: .zero, y: (self.phoneButton.frame.width + 12))
+                self.locationButton.transform = CGAffineTransform(translationX: .zero, y: ((self.phoneButton.frame.width + 12) * 2))
+                self.linkButton.transform = CGAffineTransform(translationX: .zero, y: ((self.phoneButton.frame.width + 12) * 3))
+                self.button.setImage(UIImage(named: "menu2"), for: .normal)
+                self.button.setImageTintColor(UIColor(hexFromString: "FFFFFF"))
+            } completion: { _ in
+                self.phoneButton.isHidden = true
+                self.locationButton.isHidden = true
+                self.linkButton.isHidden = true
+            }
+        }
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -32,6 +105,7 @@ class DetailViewController: UITableViewController {
         let cell = UITableViewCell()
         guard let bank = bank else { return UITableViewCell() }
         cell.textLabel?.text = String(bank.rates.usd.buy)
+        
         return cell
     }
 }
--- final-project/Currency/Currency/View/SharedButton.swift
+++ final-project/Currency/Currency/View/SharedButton.swift
@@ -19,21 +19,31 @@ enum BgColor: String {
 
 class SharedButton: UIButton {
     
+    let label: UILabel = {
+        let label = UILabel()
+        label.tintColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    init(diameter: Int, imageName: String, tintColor: TintColor, bgColor: BgColor) {
+    
+    init(diameter: Int, imageName: String, tintColor: TintColor, bgColor: BgColor, shadow: Bool = false) {
         super.init(frame: .zero)
         setupButton(diameter: CGFloat(diameter),
                     image: imageName,
                     tintColorImage: tintColor.rawValue,
-                    bgColor: bgColor.rawValue)
+                    bgColor: bgColor.rawValue,
+                    shadow: shadow)
     }
     
-    func setupButton(diameter: CGFloat,
+    private func setupButton(diameter: CGFloat,
                      image: String,
                      tintColorImage: TintColor.RawValue,
-                     bgColor: BgColor.RawValue) {
+                     bgColor: BgColor.RawValue,
+                     shadow: Bool) {
         self.translatesAutoresizingMaskIntoConstraints = false
         
         // set image
@@ -50,8 +60,8 @@ class SharedButton: UIButton {
         
         self.widthAnchor.constraint(equalToConstant: diameter).isActive = true
         self.heightAnchor.constraint(equalToConstant: diameter).isActive = true
+        shadow ? setShadow() : nil
         
-        setShadow()
     }
     
     private func setShadow() {
